54a4f9b64a8b003cd907550713bb827dc02ee54d
Carlos Vera A <carveraa13@gmail.com>
2021-02-25 06:56:17 +0000

    update

--- exercises/02.2-maping-array-of-objects-to-li/index.js
+++ exercises/02.2-maping-array-of-objects-to-li/index.js
@@ -1,5 +1,6 @@
 import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+import { element } from "prop-types";
 
 const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
 
@@ -8,7 +9,11 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * planets in html called planetsInHTML
  */
 
+const planetsInHTML = planets.map(element => {
+	return <li className="list-group-item">{element}</li>;
+});
+
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = (<ul className="list-group m-5"></ul>);
+const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
